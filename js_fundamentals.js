fdc5f67dd0f265fa7257c9adf0a824e281acf42a
Bhavin <bhavinpampaniya07@gmail.com>
2023-02-20 17:00:42 +0530

    implimented prime number function

--- js_fundamentals.js
+++ js_fundamentals.js
@@ -1,2 +1,13 @@
-console.log("hello world");
-console.log("aditya");
+//TASK 1 :- output prime numbers from 2 --> n
+let n = 11;
+for(let i=2; i<=n; i++){
+    let isPrime = true;
+    for(let j=2;j<=i;j++){
+        if(i%j==0 && i!=j ) isPrime = false; break;
+    }
+    if(isPrime) console.log(i);
+}
+ 
+
+
+
