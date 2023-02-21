361094246ebe35cb1141ef4f85a5d01f43eda167
Bhavin <bhavinpampaniya07@gmail.com>
2023-02-21 16:13:42 +0530

    practiced arrow function and use case of it and how 'this' works inside an arrow function

--- js_fundamentals.js
+++ js_fundamentals.js
@@ -11,16 +11,55 @@
 
 //destructuring NEsted
 
-const users = [
-    {id:1,name:"Bhavin"},
-    {id:2,name: "pampaniya"},
-    {id:3,name:"ms"}
-]
+// const users = [
+//     {id:1,name:"Bhavin"},
+//     {id:2,name: "pampaniya"},
+//     {id:3,name:"ms"}
+// ]
+// const [{id}, ,{name}] = users;
 
-const [{id}, ,{name}] = users;
-console.log(id);
-// console.log(obj2);
-console.log(name);
+// "use strict"
+
+// let name = "ok";
+// function normal(){
+//     let name = "bhavin";
+//     console.log("normal function \n", this);
+// }
+// // console.log("global",this);
+// const arrow = ()=>{
+//     let name = "aeewer";
+//     console.log("arrow function ",this.name);
+// }
+// normal();
+// arrow();
 
 
+const myObj = {
+    name:"bhavin",
+    id:2,
+    edu: ()=>{
+        let id = 150;
+        console.log("studing in Ganpat Uni and my id is ", this);
+    },
+    edu2: function(){
+        const objfunct = ()=>{
+            console.log("inside object ", this.id);
+        }
+        let id = 56;
+        console.log("studing in Ganpat Uni and my id is ", this);   
+        objfunct();
+    } 
+}
 
+function myFunct() {
+    let name = "bjhva9n";
+    const newfunc = ()=>{
+        console.log("my name is ",this.name);
+    }
+    newfunc();
+}
+
+myFunct();
+
+myObj.edu();
+myObj.edu2();
