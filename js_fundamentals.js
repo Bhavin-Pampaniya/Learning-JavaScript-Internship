b718d818511e8c860aa0c5f042fd82c11448c078
Bhavin <bhavinpampaniya07@gmail.com>
2023-02-21 15:32:31 +0530

    practiced destructuring in obj and arrays

--- js_fundamentals.js
+++ js_fundamentals.js
@@ -1,13 +1,26 @@
 //TASK 1 :- output prime numbers from 2 --> n
-let n = 11;
-for(let i=2; i<=n; i++){
-    let isPrime = true;
-    for(let j=2;j<=i;j++){
-        if(i%j==0 && i!=j ) isPrime = false; break;
-    }
-    if(isPrime) console.log(i);
-}
- 
+// let n = 11;
+// for(let i=2; i<=n; i++){
+//     let isPrime = true;
+//     for(let j=2;j<=i;j++){
+//         if(i%j==0 && i!=j ) isPrime = false; break;
+//     }
+//     if(isPrime) console.log(i);
+// }
+
+
+//destructuring NEsted
+
+const users = [
+    {id:1,name:"Bhavin"},
+    {id:2,name: "pampaniya"},
+    {id:3,name:"ms"}
+]
+
+const [{id}, ,{name}] = users;
+console.log(id);
+// console.log(obj2);
+console.log(name);
 
 
 
